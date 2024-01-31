60e9ac368aad8a9c7cd80a2b749a18509a562371
eduard <avas.eduard@gmail.com>
2024-01-31 19:41:17 +0200

    export and import properties

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register'
-import * as cdk from 'aws-cdk-lib'
-import { CdkStarterStack } from '../lib/cdk-starter-stack'
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+// import { CdkStarterStack } from '../lib/cdk-starter-stack'
+import { PhotosStack } from '../lib/PhotosStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
 
-const app = new cdk.App()
-new CdkStarterStack(app, 'CdkStarterStack')
+const app = new cdk.App();
+new PhotosStack(app, 'PhotosStack');
+new PhotosHandlerStack(app, 'PhotosHandlerStack');
