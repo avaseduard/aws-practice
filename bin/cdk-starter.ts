cdd43823e9d4fd4e8e727cab0eae522056cf305c
eduard <avas.eduard@gmail.com>
2024-02-01 19:06:24 +0200

    create a tag

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -4,10 +4,17 @@ import * as cdk from 'aws-cdk-lib';
 // import { CdkStarterStack } from '../lib/cdk-starter-stack'
 import { PhotosStack } from '../lib/PhotosStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
-const photosStack = new PhotosStack(app, 'PhotosStack');
 
+// Bucket stack
+const photosStack = new PhotosStack(app, 'PhotosStack');
+// Lambda stack
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
-  targetBucketArn: photosStack.photosBucketArn
+  targetBucketArn: photosStack.photosBucketArn,
 });
+// Tagger
+const tagger = new BucketTagger('level', 'test');
+// Aspects
+cdk.Aspects.of(app).add(tagger);
